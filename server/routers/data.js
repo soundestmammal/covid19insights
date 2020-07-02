375eb54e5cdf1487ff7743621d84c224a52c7180
Rob <robert.checco@gmail.com>
2020-07-01 17:41:06 -0700

    Do percent traced calculation on server side, send clean data to ui

--- server/routers/data.js
+++ server/routers/data.js
@@ -3,20 +3,23 @@
  * Provide data for the client to consume
  */
 
- const express = require('express');
- const router = express.Router();
+const express = require('express');
+const router = express.Router();
+const percentTraced = require('../services/percentTraced');
 
 const contactTrace = require('../data/contact-trace.json');
 const contactTraceState = require('../data/contract-trace-percent-by-state.json');
 const usStates = require('../data/us-states.json');
 const nysCounty = require('../data/nys-county-data.json');
 
+const contactTraceState1 = percentTraced(contactTrace, usStates);
+
 router.get('/data/contact-trace', (req,res) => {
     res.send({data: contactTrace});
 });
 
 router.get('/data/contact-trace-percent-by-state', (req,res) => {
-    res.send({data: contactTraceState});
+    res.send({data: contactTraceState1});
 });
 
 router.get('/data/us-states', (req,res) => {
