3cdb0a699ba50e6a081453f392a1254dd2c19ada
Rob <robert.checco@gmail.com>
2020-07-04 11:44:52 -0700

    Create Dashboard-summary UI.
    Still needs media queries, ability to accept dynamic data.

--- client/src/components/USMap.js
+++ client/src/components/USMap.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import StatePath from './StatePath';
 import states from '../json/states.json';
+// Load in Color Data...
 
 class USMap extends Component {
+
+    getRiskLevelColor = (riskLevel) => {
+        const riskLevelColorMap = {
+            low: "rgb(0, 212, 116)",
+            medium: "rgb(255, 201, 0)",
+            high: "rgb(255, 150, 0)",
+            outbreak: "rgb(255, 0, 52)",
+        }
+        return riskLevelColorMap[riskLevel];
+    }
+
+    getRiskLevel = (state) => {
+        const riskLevelByState = [];
+        const thisRisk = riskLevelByState[state];
+        return this.getRiskLevelColor(thisRisk)
+    }
+
     render() {
         const map = states.map((state, index) => 
             <StatePath
@@ -10,7 +28,7 @@ class USMap extends Component {
                 unique={index}
                 data={state.name}
                 nav={this.props.nav}
-                fill={'blue'}
+                fill={this.getRiskLevelColor("low")}
             > 
             </StatePath>
         );
