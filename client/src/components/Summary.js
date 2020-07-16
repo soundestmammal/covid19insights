97819478d63fb98ff3fe38624a3309eb0c3d2c07
Rob <robert.checco@gmail.com>
2020-07-16 12:12:00 -0700

    Conditionally render the Summary Content

--- client/src/components/Summary.js
+++ client/src/components/Summary.js
@@ -14,16 +14,16 @@ const Summary = (props) => {
         let riskLevel;
         if(positiveTestRate < 3) {
             subtitle = "Indicates widespread testing";
-            riskLevel = "Low";
+            riskLevel = "low";
         } else if (positiveTestRate >= 3 && positiveTestRate < 10) {
             subtitle = "Indicates adequate testing";
-            riskLevel = "Medium";
+            riskLevel = "medium";
         } else if(positiveTestRate >= 10 && positiveTestRate < 20) {
             subtitle = "Indicates insufficent testing";
-            riskLevel = "High";
+            riskLevel = "high";
         } else if(positiveTestRate >= 20 && positiveTestRate <= 100) {
             subtitle = "Indicates dangerously little testing";
-            riskLevel = "Critical";
+            riskLevel = "critical";
         } else {
             throw new Error("Positive test rate was not calculated right");
         }
@@ -33,16 +33,30 @@ const Summary = (props) => {
                 <span className="title">Positive Test Rate</span>
                 <span className="subtitle">{subtitle}</span>
                 <span className="data-value">{positiveTestRate}%</span>
-                <span className="risk">{riskLevel}</span>
+                <span className={`risk ${riskLevel}`}>{riskLevel}</span>
             </div>
         );
     }
 
     function renderStateStatus() {
+        let status;
+        if(reproductionRate >= 1.2) {
+            status = `${state} is either experiencing an outbreak or will be in the future.`;
+        } else if (reproductionRate >= 1.1 && reproductionRate < 1.2) {
+            status = `${state} is experiencing a rapid rise in cases.`;
+        } else if (reproductionRate < 1.1 && reproductionRate >= 1) {
+            status = `${state} is controlling the growth of cases.`;
+        } else if (reproductionRate < 1) {
+            status = `${state} is seeing a reduction in daily cases. The size of the outbreak is shrinking.`;
+        } else {
+            throw new Error("There was an error in rendering the State Status");
+            // Have something handle a default case catch bugs.
+        }
+
         return(
             <div className="state-status-container">
                 <span className="state-name">{state}</span>
-                <p>{state} is on track to contain COVID. Cases are steadily decreasing and {state}’s COVID preparedness meets or exceeds international standards.</p>
+                <p>{status}</p>
             </div>
         );
     }
@@ -60,14 +74,50 @@ const Summary = (props) => {
 
     function renderReproductionRateContent() {
 
-        // if(reproductionRate > )
+        // I need to do a few things here
 
+        // 1. I need to conditionally render the subtitle.
+        let subtitle;
+        if(reproductionRate < 1) {
+            subtitle = "The number of daily cases is decreasing";
+        } else {
+            subtitle = "The number of daily cases is increasing";
+        }
+        console.log("Subtitle", subtitle);
         return(
             <div className="indicator-card-content">
                 <span className="title">Reproduction rate</span>
-                <span className="subtitle">The number of daily cases are decreasing</span>
+                <span className="subtitle">{subtitle}</span>
                 <span className="data-value">{reproductionRate}</span>
-                <span className="risk">Low</span>
+                <span className={`risk ${riskLevel}`} >{riskLevel}</span>
+            </div>
+        );
+    }
+
+    function renderContactTraceRateContent() {
+        console.log("Contact Trace Rate", contactTraceRate);
+        let subtitle;
+        let riskLevel;
+        if(contactTraceRate >= 90) {
+            subtitle = "Contact tracing level is sufficient"
+            riskLevel = "low";
+        } else if(contactTraceRate < 90 && contactTraceRate >= 20) {
+            subtitle = "Contact tracing level is fair";
+            riskLevel = "medium";
+        } else if (contactTraceRate >= 3 && contactTraceRate < 20) {
+            subtitle = "Contact tracing level is failing";
+            riskLevel = "high";
+        } else if (contactTraceRate < 3) {
+            subtitle = "Contact tracing has collapsed";
+            riskLevel = "critical";
+        }
+
+        return(
+            <div className="indicator-card-content">
+                <span className="title">Contact Trace Rate</span>
+                <span className="subtitle">{subtitle}</span>
+                <span className="data-value">{contactTraceRate}%</span>
+                <span className={`risk ${riskLevel}`}>{riskLevel}</span>
             </div>
         );
     }
@@ -82,23 +132,13 @@ const Summary = (props) => {
             </div>
             <div className="indicator-container">
                 <div className="indicator-card">
-                    <div className="indicator-card-content">
-                        <span className="title">Reproduction rate</span>
-                        <span className="subtitle">The number of daily cases are decreasing</span>
-                        <span className="data-value">{reproductionRate}</span>
-                        <span className="risk">Low</span>
-                    </div>
+                    {renderReproductionRateContent()}
                 </div>
                 <div className="indicator-card">
                     {renderPositiveTestRateContent()}
                 </div>
                 <div className="indicator-card">
-                    <div className="indicator-card-content">
-                        <span className="title">Contact Trace Rate</span>
-                        <span className="subtitle">The number of daily cases is decreasing</span>
-                        <span className="data-value">{contactTraceRate}%</span>
-                        <span className="risk">Low</span>
-                    </div>
+                    {renderContactTraceRateContent()}
                 </div>
             </div>
             <div className="last-updated">
