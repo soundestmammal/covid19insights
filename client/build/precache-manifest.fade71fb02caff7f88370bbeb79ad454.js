d8057f80dd748a6e40f9091e822669efcc997a58
Rob <robert.checco@gmail.com>
2020-08-31 14:44:22 -0400

    Hot fix - Server unable to build project, build locally

---
+++ client/build/precache-manifest.fade71fb02caff7f88370bbeb79ad454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080b6c507e7195746a70a1c665c46d88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f8a246397dda76ada1b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d4299985888003a676ec",
+    "url": "/static/css/main.06011ad4.chunk.css"
+  },
+  {
+    "revision": "3f8a246397dda76ada1b",
+    "url": "/static/js/2.387020bf.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/static/js/2.387020bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4299985888003a676ec",
+    "url": "/static/js/main.7c3348b2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
