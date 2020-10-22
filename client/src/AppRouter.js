594bfa2cb13a73854a4241f23e5ee3e9755c7649
Rob <robert.checco@gmail.com>
2020-10-21 21:28:09 -0400

    Add new data schema to client and begin restructure how data flows down

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -8,20 +8,22 @@ import Indicators from './components/Indicators';
 import USMap from './components/USMap';
 import Dashboard from './components/Dashboard';
 import About from './components/About';
+import data from './json/join.json';
 
 class AppRouter extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentState: '',
-      summary: '',
-      dailyCases: '',
-      dailyCasesMA: '',
-      dailyDeaths: '',
-      dailyDeathsMA: '',
-      reproductionRate: '',
-      positiveTestRate: '',
-      contactTraceRate: '',
+      data: ''
+      // summary: '',
+      // dailyCases: '',
+      // dailyCasesMA: '',
+      // dailyDeaths: '',
+      // dailyDeathsMA: '',
+      // reproductionRate: '',
+      // positiveTestRate: '',
+      // contactTraceRate: '',
     };
   }
 
@@ -31,42 +33,43 @@ class AppRouter extends Component {
 
   // Fetch Data for the application
   // This is a temporary solution. This will be one fetch, one setState in the future.
-  fetchData = async () => {
-    const summary = await axios.get('/api/v1/summary');
-    const dailyCases = await axios.get(
-      '/api/v1/daily_cases'
-    );
-    const dailyCasesMA = await axios.get(
-      '/api/v1/daily_cases_moving_average'
-    );
-    const dailyDeaths = await axios.get(
-      '/api/v1/daily_deaths'
-    );
-    const dailyDeathsMA = await axios.get(
-      '/api/v1/daily_deaths_moving_average'
-    );
-    const positiveTestRate = await axios.get(
-      '/api/v1/positive_test_rate'
-    );
-    const reproductionRate = await axios.get(
-      '/api/v1/reproduction_rate'
-    );
-    const contactTraceRate = await axios.get(
-      '/api/v1/contact_trace_rate'
-    );
-    const riskLevel = await axios.get(
-      '/api/v1/risk_level'
-    );
+  fetchData = () => {
+    // const summary = await axios.get('/api/v1/summary');
+    // const dailyCases = await axios.get(
+    //   '/api/v1/daily_cases'
+    // );
+    // const dailyCasesMA = await axios.get(
+    //   '/api/v1/daily_cases_moving_average'
+    // );
+    // const dailyDeaths = await axios.get(
+    //   '/api/v1/daily_deaths'
+    // );
+    // const dailyDeathsMA = await axios.get(
+    //   '/api/v1/daily_deaths_moving_average'
+    // );
+    // const positiveTestRate = await axios.get(
+    //   '/api/v1/positive_test_rate'
+    // );
+    // const reproductionRate = await axios.get(
+    //   '/api/v1/reproduction_rate'
+    // );
+    // const contactTraceRate = await axios.get(
+    //   '/api/v1/contact_trace_rate'
+    // );
+    // const riskLevel = await axios.get(
+    //   '/api/v1/risk_level'
+    // );
 
-    this.setState({ summary: summary.data });
-    this.setState({ dailyCases: dailyCases.data });
-    this.setState({ dailyCasesMA: dailyCasesMA.data });
-    this.setState({ dailyDeaths: dailyDeaths.data });
-    this.setState({ dailyDeathsMA: dailyDeathsMA.data });
-    this.setState({ positiveTestRate: positiveTestRate.data });
-    this.setState({ reproductionRate: reproductionRate.data });
-    this.setState({ contactTraceRate: contactTraceRate.data });
-    this.setState({ riskLevel: riskLevel.data });
+    // this.setState({ summary: summary.data });
+    // this.setState({ dailyCases: dailyCases.data });
+    // this.setState({ dailyCasesMA: dailyCasesMA.data });
+    // this.setState({ dailyDeaths: dailyDeaths.data });
+    // this.setState({ dailyDeathsMA: dailyDeathsMA.data });
+    // this.setState({ positiveTestRate: positiveTestRate.data });
+    // this.setState({ reproductionRate: reproductionRate.data });
+    // this.setState({ contactTraceRate: contactTraceRate.data });
+    // this.setState({ riskLevel: riskLevel.data });
+    this.setState({ data: data });
   };
 
   componentDidMount() {
@@ -75,7 +78,7 @@ class AppRouter extends Component {
 
   render() {
     // Wait for the data to be fetched...
-    if (this.state.summary === '') return null;
+    if (this.state.data === '') return null;
     console.log(this.state);
     return (
       <div>
@@ -90,10 +93,10 @@ class AppRouter extends Component {
               style={{ marginTop: '56px' }}
             />
             <Indicators style={{ marginTop: '56px' }} />
-            <USMap nav={this.navToState} data={this.state.summary} />
+            <USMap nav={this.navToState} data={this.state.data} />
           </Route>
           <Route path='/detail'>
-            <Dashboard state={this.state.currentState} data={this.state} />
+            <Dashboard state={this.state.currentState} data={this.state.data[this.state.currentState]} />
           </Route>
           <Route path='/about'>
             <About />
